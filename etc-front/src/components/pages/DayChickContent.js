1594fbbd54339e737e5452b614420007799278e3
chernet536 <56545626+chernet536@users.noreply.github.com>
2023-09-24 13:43:33 +0300

    Update DayChickContent.js

--- etc-front/src/components/pages/DayChickContent.js
+++ etc-front/src/components/pages/DayChickContent.js
@@ -4,7 +4,7 @@ function DayChickContent() {
   return (
     <div className='container8'>
        <div className='paragraph5'>
-         <h1>BOVANS BROWN</h1>
+         <h1>Brown Egg Layer Chicken</h1>
          <p>A commercial layer purely for commercial egg production under controlled egg production infrastructure with formulated feeding regimes only.</p>
        </div>
        <div className='paragraph6'>
